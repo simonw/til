db142ddecb549cf0d3f870dea7fced052f0e10bd
Simon Willison <swillison@gmail.com>
2020-04-19 09:20:29 -0700

    Script for updating the README with an index, refs #1

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # til
 
 My Today I Learned snippets. Inspired by [jbranchaud/til](https://github.com/jbranchaud/til), which I spotted [on Hacker News](https://news.ycombinator.com/item?id=22908044).
+
+<!-- index starts -->
+<!-- index ends -->
---
+++ update_readme.py
@@ -0,0 +1,35 @@
+"Run this after build_database.py - it needs til.db"
+import pathlib
+import sqlite_utils
+import sys
+import re
+
+root = pathlib.Path(__file__).parent.resolve()
+
+index_re = re.compile(r"<!\-\- index starts \-\->.*<!\-\- index ends \-\->", re.DOTALL)
+
+if __name__ == "__main__":
+    db = sqlite_utils.Database(root / "til.db")
+    by_topic = {}
+    for row in db["til"].rows_where(order_by="created_utc"):
+        by_topic.setdefault(row["topic"], []).append(row)
+    index = ["<!-- index starts -->"]
+    for topic, rows in by_topic.items():
+        index.append("## {}\n".format(topic))
+        for row in rows:
+            index.append(
+                "* [{title}]({url}) - {date}".format(
+                    date=row["created"].split("T")[0], **row
+                )
+            )
+        index.append("")
+    if index[-1] == "":
+        index.pop()
+    index.append("<!-- index ends -->")
+    if "--rewrite" in sys.argv:
+        readme = root / "README.md"
+        index_txt = "\n".join(index).strip()
+        readme_contents = readme.open().read()
+        readme.open("w").write(index_re.sub(index_txt, readme_contents))
+    else:
+        print("\n".join(index))
