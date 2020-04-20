fb1dbf82c290b0948eb79adff6e5950cc48384e2
Simon Willison <swillison@gmail.com>
2020-04-20 12:52:07 -0700

    Use GFM in atom feed, refs #6

--- plugins/sql_functions.py
+++ plugins/sql_functions.py
@@ -1,7 +1,20 @@
 from datasette import hookimpl
 from datasette_render_markdown import render_markdown
 
+def _render_markdown(s):
+    return render_markdown(
+        s,
+        extra_tags=["img", "hr", "br", "details", "summary", "input", "div", "span"],
+        extra_attrs={
+            "input": ["type", "disabled", "checked"],
+            "img": ["src"],
+            "div": ["class"],
+            "span": ["class"],
+        },
+        extensions=["mdx_gfm:GithubFlavoredMarkdownExtension"],
+    )
+
 
 @hookimpl
 def prepare_connection(conn):
-    conn.create_function("render_markdown", 1, render_markdown)
+    conn.create_function("render_markdown", 1, _render_markdown)
