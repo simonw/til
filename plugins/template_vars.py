51f5daef61b6bbe6c5be564b8644d2bff6761ab0
Simon Willison <swillison@gmail.com>
2020-07-14 10:52:43 -0700

    Add snippet highlighting to search

--- plugins/template_vars.py
+++ plugins/template_vars.py
@@ -1,6 +1,16 @@
 from datasette import hookimpl
+import html
+
+
+def highlight(s):
+    s = html.escape(s)
+    s = s.replace("b4de2a49c8", "<strong>").replace("8c94a2ed4b", "</strong>")
+    return s
 
 
 @hookimpl
 def extra_template_vars(request):
-    return {"q": request.args.get("q", "")}
+    return {
+        "q": request.args.get("q", ""),
+        "highlight": highlight,
+    }
