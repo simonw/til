8fc2f462b86c7f49fbfb33eb70c4d12d0178705d
Simon Willison <swillison@gmail.com>
2020-08-22 13:23:41 -0700

    Removed render_markdown() SQL function, refs #19

--- plugins/sql_functions.py
+++ plugins/sql_functions.py
@@ -1,22 +1,6 @@
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from datasette import hookimpl
-from datasette_render_markdown import render_markdown
-
-
-def _render_markdown(s):
-    return render_markdown(
-        s,
-        extra_tags=["img", "hr", "br", "details", "summary", "input", "div", "span",],
-        extra_attrs={
-            "input": ["type", "disabled", "checked"],
-            "img": ["src", "alt"],
-            "div": ["class"],
-            "span": ["class"],
-            "a": ["href"],
-        },
-        extensions=["mdx_gfm:GithubFlavoredMarkdownExtension"],
-    )
 
 
 def rewrite_github_images(html, base_url):
@@ -29,5 +13,4 @@ def rewrite_github_images(html, base_url):
 
 @hookimpl
 def prepare_connection(conn):
-    conn.create_function("render_markdown", 1, _render_markdown)
     conn.create_function("rewrite_github_images", 2, rewrite_github_images)
