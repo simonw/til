06e970a73556130a25110096e148ae16680777c5
Simon Willison <swillison@gmail.com>
2022-07-31 17:18:25 -0700

    Show related TILs on every page, refs #50

--- plugins/template_vars.py
+++ plugins/template_vars.py
@@ -1,6 +1,10 @@
 from datasette import hookimpl
 from bs4 import BeautifulSoup as Soup
 import html
+import re
+
+non_alphanumeric = re.compile(r"[^a-zA-Z0-9\s]")
+multi_spaces = re.compile(r"\s+")
 
 
 def first_paragraph(html):
@@ -15,9 +19,28 @@ def highlight(s):
 
 
 @hookimpl
-def extra_template_vars(request):
+def extra_template_vars(request, datasette):
+    async def related_tils(til):
+        text = til["title"] + " " + til["body"]
+        text = non_alphanumeric.sub(" ", text)
+        text = multi_spaces.sub(" ", text)
+        words = list(set(text.lower().strip().split()))
+        sql = """
+        select til.topic, til.slug, til.title, til.created, til.html
+        from til join til_fts on til.rowid = til_fts.rowid
+        where til_fts match :words
+        and not (til.slug = :slug and til.topic = :topic)
+        order by til_fts.rank limit 5
+        """
+        result = await datasette.get_database().execute(
+            sql,
+            {"words": " OR ".join(words), "slug": til["slug"], "topic": til["topic"]},
+        )
+        return result.rows
+
     return {
         "q": request.args.get("q", ""),
         "highlight": highlight,
         "first_paragraph": first_paragraph,
+        "related_tils": related_tils,
     }
